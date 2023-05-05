712ba0c344674c6b321852ae6d0c1ad5f5cf9547
Byron Calvete <calvete94@gmail.com>
2023-05-05 14:32:28 -0300

    add: citas page

--- app/citas/page.jsx
+++ app/citas/page.jsx
@@ -1,25 +1,46 @@
 import Image from 'next/image'
 import Link from 'next/link'
 
+const Component = ({ name }) => {
+  return (
+    <li className="w-full flex items-center py-3 px-4 bg-white dark:bg-slate-200 rounded-lg shadow-md border border-black hover:border-yc-red">
+      <Image width={50} height={50} src="/image-icon.svg" alt="Pendiente imagen"/>
+      <h4 className="dark:text-slate-900">{name}</h4>
+    </li>
+  )
+}
+
 export default function Citas() {
+  const appointments = [
+    {
+      id: 1,
+      name: 'Toma de huellas',
+    },
+    {
+      id: 2,
+      name: 'Recogida de TIE',
+    },
+    {
+      id: 3,
+      name: 'Canje de licencia',
+    },
+    {
+      id: 4,
+      name: 'Nueva normalidad',
+    }
+  ]
+
   return (
     <main>
       <div className="bg-hero dark:bg-none dark:bg-[#363939]">
-        <section id="hero" className="max-w-5xl mx-auto flex flex-col-reverse justify-center sm:flex-row p-6 items-center gap-8 sm:gap-10 mb-12 scroll-mt-40 widescreen:section-min-height tallscreen:section-min-height">
-          <article className="sm:w-2/3">
-            <h2 className="max-w-lg text-4xl font-bold text-left sm:text-5xl text-slate-900 dark:text-white">
-              We Boldy Go <span className="text-yc-red dark:opacity-80">Where No Rocket</span> Has Gone Before...
-            </h2>
-            <p className="max-w-md text-2xl mt-4 text-left text-slate-700 dark:text-slate-400">
-              We&apos;re building rockets for the next century today. From the moon to Mars, Jupiter and beyond<br/><br/>Think Acme Rockets.
-            </p>
-            <div className="mt-8 text-center sm:text-left">
-              <button className="bg-yc-red px-3 py-4 text-md md:text-xl rounded-lg hover:opacity-90 text-white w-full md:w-2/3 sm:w-5/6 border border-black shadow-md hover:shadow-xl"><Link href='/certificado-digital'>Obtener certificado</Link></button>
-            </div>
-          </article>
-          <div className="w-1/2 sm:w-1/3 h-60 sm:h-80 grid place-content-center border-solid border-4 border-black dark:border-white rounded-xl bg-slate-200">
-            <Image width={150} height={150} src="/image-icon.svg" alt="Pendiente imagen" />
-          </div>
+        <section id="hero" className="max-w-5xl mx-auto flex flex-col justify-center p-6 items-center gap-8 sm:gap-10 scroll-mt-40 widescreen:section-min-height tallscreen:section-min-height">
+          <h2 className="text-4xl font-bold text-left sm:text-5xl text-slate-900 dark:text-white mb-2 sm:mb-6">
+            Te ayudamos con las citas de extranjería
+          </h2>
+          <ul className="w-full grid grid-cols-1 sm:grid-cols-3 gap-8 px-2 sm:px-12">
+            {appointments.map(appointment => <Component key={appointment.id} name={appointment.name}/>)}
+          </ul>
+          <button></button>
         </section>
       </div>
     </main>
