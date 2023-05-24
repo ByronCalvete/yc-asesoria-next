f060aede3bc0af8adc493ef27756320382ec2624
Byron Calvete <calvete94@gmail.com>
2023-05-24 17:27:53 -0300

    add: description in familiar comunitario's page

--- app/residencias/familiar-comunitario/page.jsx
+++ app/residencias/familiar-comunitario/page.jsx
@@ -23,7 +23,17 @@ const data = {
       id: 5,
       content: 'Cumplir con los requisitos de seguridad y salud pública.'
     },
-  ]
+  ],
+  comments: [
+    {
+      id: 1,
+      content: 'El proceso de solicitud de la residencia por familiar comunitario se realiza ante la Oficina de Extranjería correspondiente al lugar de residencia en España. Es importante seguir los procedimientos establecidos y presentar toda la documentación requerida para completar el trámite.',
+    },
+    {
+      id: 2,
+      content: 'Una vez obtenida la tarjeta de residencia, el familiar comunitario puede vivir y trabajar legalmente en España durante el periodo establecido en el permiso, el cual suele estar vinculado a la duración de la residencia del ciudadano de la UE o EEE en el país.',
+    }
+  ],
 }
 
 export default function FamiliarComunitario () {
@@ -31,12 +41,17 @@ export default function FamiliarComunitario () {
     <>
       <section className="max-w-5xl my-6 grid mx-auto p-6 gap-4 sm:gap-6">
         <h2 className="text-3xl font-bold text-center sm:text-5xl mb-2 sm:mb-6 text-yc-red">Residencia por familiar comunitario</h2>
-        <p className="font-normal text-2xl text-center sm:text-left mb-2 sm:mb-6">{data.description}</p>
+        <p className="font-normal text-xl sm:text-2xl text-justify mb-2 sm:mb-6">{data.description}</p>
         {data.requirements.map(t => (
           <div key={t.id} className="flex items-start">
             <span className="font-light px-4 text-xl">✔️</span>
-            <p className="font-light text-lg sm:text-xl">{t.content}</p>
+            <p className="font-light text-lg sm:text-xl underline underline-offset-8 decoration-yc-red">{t.content}</p>
           </div>
+        ))}
+      </section>
+      <section className="max-w-5xl my-6 grid mx-auto p-6 gap-4 sm:gap-6">
+        {data.comments.map(t => (
+          <p key={t.id} className="font-normal text-xl sm:text-2xl text-justify mb-2 sm:mb-6">{t.content}</p>
         ))}
       </section>
 
