693ba73cbb4ac29f93a34d6008606a2081b6deab
Byron Calvete <calvete94@gmail.com>
2023-05-25 20:04:24 -0300

    fix: font-weight

--- app/residencias/familiar-comunitario/page.jsx
+++ app/residencias/familiar-comunitario/page.jsx
@@ -52,11 +52,13 @@ const data = {
 export default function FamiliarComunitario () {
   return (
     <>
-      <section className="max-w-5xl my-6 grid mx-auto p-6 gap-4 sm:gap-6">
+      <section className="max-w-5xl my-2 sm:my-6 grid mx-auto p-6 gap-4 sm:gap-6">
         <h2 className="text-4xl font-bold text-center sm:text-5xl mb-2 sm:mb-6 text-yc-red">Residencia por familiar comunitario</h2>
         {data.description.map(t => (
           <p key={t.id} className="font-normal text-xl sm:text-2xl text-justify">{t.content}</p>
         ))}
+      </section>
+      <section className="max-w-5xl my-2 sm:my-6 grid mx-auto p-6 gap-4 sm:gap-6">
         {data.requirements.map(t => (
           <div key={t.id} className="flex items-start">
             <span className="font-light px-4 text-xl">✔️</span>
@@ -64,7 +66,7 @@ export default function FamiliarComunitario () {
           </div>
         ))}
       </section>
-      <section className="max-w-5xl my-6 grid mx-auto p-6 gap-4 sm:gap-6">
+      <section className="max-w-5xl my-2 sm:my-6 grid mx-auto p-6 gap-4 sm:gap-6">
         {data.comments.map(t => (
           <p key={t.id} className="font-normal text-xl sm:text-2xl text-justify">{t.content}</p>
         ))}
--- app/residencias/no-lucrativa/page.jsx
+++ app/residencias/no-lucrativa/page.jsx
@@ -48,11 +48,13 @@ const data = {
 export default function ResidenciaNoLucrativa () {
   return (
     <>
-      <section className="max-w-5xl my-6 grid mx-auto p-6 gap-4 sm:gap-6">
+      <section className="max-w-5xl my-2 sm:my-6 grid mx-auto p-6 gap-4 sm:gap-6">
         <h2 className="text-4xl font-bold text-center sm:text-5xl mb-2 sm:mb-6 text-yc-red">Residencia no lucrativa</h2>
         {data.description.map(t => (
           <p key={t.id} className="font-normal text-xl sm:text-2xl text-justify">{t.content}</p>
         ))}
+      </section>
+      <section className="max-w-5xl my-2 sm:my-6 grid mx-auto p-6 gap-4 sm:gap-6">
         {data.requirements.map(t => (
           <div key={t.id} className="flex items-start">
             <span className="font-light px-4 text-xl">✔️</span>
@@ -60,7 +62,7 @@ export default function ResidenciaNoLucrativa () {
           </div>
         ))}
       </section>
-      <section className="max-w-5xl my-6 grid mx-auto p-6 gap-4 sm:gap-6">
+      <section className="max-w-5xl my-2 sm:my-6 grid mx-auto p-6 gap-4 sm:gap-6">
         {data.comments.map(t => (
           <p key={t.id} className="font-normal text-xl sm:text-2xl text-justify">{t.content}</p>
         ))}
